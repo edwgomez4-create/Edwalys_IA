780d96210bca8f2a4940daebb9f6457d3047f7d9
edwgomez4-create <edwgomez4@gmail.com>
2026-09-05 00:22:16 -0400

    Update App.py

--- App.py
+++ App.py
@@ -2,6 +2,6 @@ import gradio
 
 def generar():
          return "¡Hola! Tu IA está funcionando."
-     demo = gradio.Interface(fn=generar, inputs="text", outputs="text")
+demo = gradio.Interface(fn=generar, inputs="text", outputs="text")
 demo.launch()
 
